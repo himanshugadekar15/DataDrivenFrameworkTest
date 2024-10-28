bf3beae9d691753bcf7eec9d55949a698e72c25b
Himanshu Gadekar <himanshugadekar15@gmail.com>
2024-10-28 23:54:20 +0530

    First Commit

---
+++ src/test/java/com/infy/base/TestBase.java
@@ -0,0 +1,46 @@
+package com.infy.base;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+
+import org.testng.annotations.*;
+
+
+public class TestBase {
+
+	public static WebDriver driver;
+	public static Properties OR = new Properties();
+	public static Properties config = new Properties();
+	public static FileInputStream fis;
+	
+	@BeforeSuite
+	public void setUp() throws IOException
+	{
+		if(driver== null)
+		{
+		FileInputStream fis = new FileInputStream("C:\\Users\\himan\\eclipse-workspace\\DataDrivenFramework\\src\\test\\resources\\properties\\config.properties");
+		FileInputStream fis2 = new FileInputStream(System.getProperty("user.dir")+"\\src\\test\\resources\\properties\\OR.properties");
+		config.load(fis);
+		OR.load(fis2);
+		
+		System.out.println(config.getProperty("browser"));
+		System.out.println(OR.getProperty("customerLogin_btn"));
+		
+		
+		
+		
+		
+		}
+	}
+	
+	@AfterSuite
+	public void tearDown()
+	{
+		
+		
+	}
+}
---
+++ src/test/java/com/infy/rough/testProperties.java
@@ -0,0 +1,30 @@
+package com.infy.rough;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class testProperties {
+
+	public static void main(String[] args) throws IOException {
+		
+//		System.out.println(System.getProperty("user.dir"));
+//		
+//		Properties OR = new Properties();
+//		Properties config = new Properties();
+//		
+//		FileInputStream fis = new FileInputStream("C:\\Users\\himan\\eclipse-workspace\\DataDrivenFramework\\src\\test\\resources\\properties\\config.properties");
+//		FileInputStream fis2 = new FileInputStream(System.getProperty("user.dir")+"\\src\\test\\resources\\properties\\OR.properties");
+//		
+//		config.load(fis);
+//		OR.load(fis2);
+//		
+//		System.out.println(config.getProperty("browser"));
+//		System.out.println(OR.getProperty("customerLogin_btn"));
+		
+	}
+	
+	
+	
+}
